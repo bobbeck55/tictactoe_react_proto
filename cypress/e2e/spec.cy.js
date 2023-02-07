a2c36602bff5f3ae7217d74a3f93b428182ace4b
bobbeck55 <bob_beck@sil.org>
2023-02-07 11:19:05 -0500

    1. finish cypress updates, 2. added percy snapshots

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,40 +1,43 @@
 
 import React from 'react'
 import Game from '../../src/Game'
-//import TestWinner from '../support/TestWinner'
-
+import '@percy/cypress'
 
 describe('spec.cy.js', () => {
 
   beforeEach(() => {
-
     cy.visit('http://localhost:3000');
-
     cy.contains('Next Player: X');
-
     cy.url().should('include', 'localhost');
-
   })
 
   it ('Winner: X', () => {
-
-    const winnerX = [2, 4, 8, 7, 9]
-
-    expect(cy.TestWinner(winnerX, "Winner: X")).eq(true);
-
+    const winnerX = [2, 4, 8, 7, 5];
+    expect(cy.TestWinner(winnerX, "Winner: X")).not.equal(null);
+    cy.percySnapshot();
   })
 
 
   it ('Winner: O', () => {
-
-
-    const winnerO = [1, 2, 0, 5, 4, 8];
-
-    expect(cy.TestWinner(winnerO, "Winner: O")).eq(null);
-
+    const winnerO = [1, 2, 0, 4, 6, 8, 7, 6, 8];
+    //    const winnerO = [1, 2, 0, 5, 4, 7, 6, 8];
+    expect(cy.TestWinner(winnerO, "Winner: O")).not.equal(null);
+    cy.percySnapshot();
   })
 
 
+  it ('Draw', () => {
+    const draw = [1, 5, 7, 4, 3, 6, 2, 0, 8];
+    expect(cy.TestWinner(draw, "Draw")).not.equal(null);
+    cy.percySnapshot();
+  })
+
+  it ('Toggle Move History Order', () => {
+    const toggle = [1, 2, 0, 5];
+    cy.TestWinner(toggle, "Next Player: X");
+    cy.get('button[type=submit]').should('have.text', 'Toggle Move History Order').click();
+    cy.percySnapshot();
+  })
 
 
 })
