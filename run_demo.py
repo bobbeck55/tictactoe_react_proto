42f7b151b3fed78468bb231d867326e96757b3a7
bobbeck55 <bob_beck@sil.org>
2023-02-09 12:43:53 -0500

    add percy env variables

--- run_demo.py
+++ run_demo.py
@@ -3,15 +3,21 @@
 #  Run demo - cypress test of tictactoe react app
 #  argv[1] - cypress o(pen) / r(un)
 #  argv[2] - percy -> run with percy
+#  argv[3] - PERCY_BRANCH=master|local
+#  argv[4] - PERCH_TOKEN=<from percy.io>
 #
 #
 import subprocess
 import sys
 import os
 
-if len(sys.argv) != 2 and len(sys.argv) != 3:
+if len(sys.argv) != 2 and len(sys.argv) != 5:
     print("Unsupported number of arguments: ", len(sys.argv))
     sys.exit(-1)
+
+myEnv = os.environ
+myEnv["PERCY_BRANCH"] = sys.argv[3]
+myEnv["PERCY_TOKEN"] = sys.argv[4]
 
 subprocess.check_output("git checkout demo_v1", shell=True)
 
@@ -29,14 +35,14 @@ else:
     print("Invalid cypress option: ", sys.argv[1])
     sys.exit(-1)
 
-if len(sys.argv) == 3 and sys.argv[2] == 'percy':
+if len(sys.argv) == 5 and sys.argv[2] == 'percy':
     cmd = r'node_modules\\.bin\\percy'
     arg1 = r'exec'
     arg2 = r'--'
     arg3 = cypressCmd
     arg4 = cypressArg1
 
-subprocess.Popen([cmd, arg1, arg2, cypressCmd, cypressArg1], shell=True)
+subprocess.Popen([cmd, arg1, arg2, cypressCmd, cypressArg1], env=myEnv, shell=True)
 
 
 
