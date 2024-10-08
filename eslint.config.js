e7a1968973217c3f20c9dc9ecec89dc80c5ccaef
bobbeck55 <107821526+bobbeck55@users.noreply.github.com>
2024-10-08 16:19:51 -0400

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        rules: {
+            eqeqeq: "off",
+            "no-unused-vars": "error",
+            "prefer-const": ["error", { "ignoreReadBeforeAssign": true }]
+        }
+    }
+];
