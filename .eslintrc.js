2225d3d10808299956476ae27eaf55f764d64ea2
bobbeck55 <107821526+bobbeck55@users.noreply.github.com>
2024-10-08 15:05:40 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
-{
-    "rules": {
-        "no-unused-vars": ["error", {
-            "vars": "all",
-            "args": "after-used",
-            "caughtErrors": "all",
-            "ignoreRestSiblings": false,
-            "reportUsedIgnorePattern": false
-        }]
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "warn"
+        }
     }
-}
+];
